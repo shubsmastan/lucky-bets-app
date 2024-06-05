aa9fb0f26e08cbe045d02e6ee28560be516ef3ec
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2024-06-06 00:01:58 +0100

    Small style changes

--- src/components/MarketsCard.tsx
+++ src/components/MarketsCard.tsx
@@ -50,7 +50,7 @@ export const MarketCard = ({ market, type }: Props) => {
 	const contractList = contracts?.map((contract: RecordType) => (
 		<div
 			key={contract.id}
-			className='flex gap-10 justify-evenly items-center py-5 border-solid border-b-zinc-600 border-b-[1px]'>
+			className='flex gap-10 justify-evenly items-center py-5 border-solid border-b-zinc-400 dark:border-b-zinc-600 border-b-[1px]'>
 			<h3 className='flex-1 ml-3'>{contract.name}</h3>
 			<div className='bg-emerald-500 rounded-md w-20 text-center px-3 py-1 font-bold'>
 				{Math.floor(Math.random() * 10000) / 100}%
@@ -64,7 +64,7 @@ export const MarketCard = ({ market, type }: Props) => {
 	return (
 		<>
 			<li className='list-none flex flex-col gap-1 p-5 bg-zinc-200 dark:bg-zinc-900 rounded-sm'>
-				<h2 className='font-bold text-xl pb-3 border-solid border-b-zinc-600 border-b-[1px]'>
+				<h2 className='font-bold text-xl pb-3 border-solid border-b-zinc-400 dark:border-b-zinc-600 border-b-[1px]'>
 					{market.name}
 				</h2>
 				{contractList}
